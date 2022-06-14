d5784813eeee6f9b7dd133d77a07af392e732e3d
HugoHanashiro <hugo.galdino@sptech.school>
2022-06-14 04:55:14 -0300

    ajustes finais

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -11,3 +11,35 @@ function checkLogin() {
         window.location = "./sign-in.html";
     }
 }
+
+function translateStatusList(statusList) {
+    let statusListTranslated = [];
+    for (let i = 0; i < statusList.length; i++) {
+        if (statusList[i] == 'wish') {
+            statusListTranslated.push('Desejo');
+        } else if (statusList[i] == 'beaten') {
+            statusListTranslated.push('Terminado');
+        } else if (statusList[i] == 'always') {
+            statusListTranslated.push('Recorrente');
+        } else if (statusList[i] == 'playing') {
+            statusListTranslated.push('Jogando');
+        } else if (statusList[i] == 'played') {
+            statusListTranslated.push('Jogado');
+        }
+    }
+    return statusListTranslated;
+}
+
+function translateStatus(status) {
+    if (status == 'wish') {
+        return 'Desejo';
+    } else if (status == 'beaten') {
+        return 'Terminado';
+    } else if (status == 'always') {
+        return 'Recorrente';
+    } else if (status == 'playing') {
+        return 'Jogando';
+    } else if (status == 'played') {
+        return 'Jogado';
+    }
+}
